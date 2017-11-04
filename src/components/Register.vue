d6c876ce510ac9cdf8b7f3253e28f98ec197a3f0
Gerardo Enriquez <genriquez1@gmail.com>
2017-11-04 13:56:47 -0700

    midterm files

---
+++ src/components/Register.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+      <form action="/users" method="post" >
+      <div class="form-group">
+        <input type="text" class="form-control" name="displayName" placeholder="Display name" v-model="displayName">
+      </div>
+      <div class="form-group">
+        <input type="text" class="form-control" name="username" placeholder="Username" v-model="username">
+      </div>
+      <div class="form-group">
+       <input type="text" class="form-control" name="email" placeholder="Email" v-model="email">
+      </div>
+      <div class="form-group">
+        <input type="password" class="form-control" name="password" placeholder="Password" v-model="password">
+      </div>
+      <div class="form-group">
+        <input type="password" class="form-control" name="passwordConf" placeholder="Confirm password" v-model="passwordConf">
+      </div>
+      <button class="btn btn-success" type="submit" >Register</button>
+      </form>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data() {
+    return {
+      displayName: '',
+      username: '',
+      email: '',
+      password: '',
+      passwordConf: ''
+    }
+  },
+  methods: {
+    registerUser() {
+      const newUser = {
+        displayName: this.displayName,
+        username: this.username,
+        email: this.email,
+        password: this.password,
+        passwordConf: this.passwordConf,
+      }
+      this.$store.dispatch('register', newUser)      
+    }
+  }
+}
+</script>
